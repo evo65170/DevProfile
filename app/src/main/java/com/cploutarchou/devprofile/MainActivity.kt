d9949e25c03fb51e0bbf346f3700ff14df7d64c1
Christos Ploutarchou <cploutarchou@gmail.com>
2019-10-09 22:33:08 +0300

    add corner radius on logo

--- app/src/main/java/com/cploutarchou/devprofile/MainActivity.kt
+++ app/src/main/java/com/cploutarchou/devprofile/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.cploutarchou.devprofile
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.logo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+//        rounded.isCircular = true
+        logo.setImageDrawable(rounded)
 
 
     }
