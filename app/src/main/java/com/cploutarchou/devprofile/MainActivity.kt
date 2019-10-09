0f2009a1e84f6aa63ad6c6c8e9b3d6012f864005
Christos Ploutarchou <cploutarchou@gmail.com>
2019-10-09 23:48:49 +0300

    create new constrain layout for x-large screens

--- app/src/main/java/com/cploutarchou/devprofile/MainActivity.kt
+++ app/src/main/java/com/cploutarchou/devprofile/MainActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
         rounded.cornerRadius = 15f
 //        rounded.isCircular = true
-        logo.setImageDrawable(rounded)
+//        logo.setImageDrawable(rounded)
 
 
     }
